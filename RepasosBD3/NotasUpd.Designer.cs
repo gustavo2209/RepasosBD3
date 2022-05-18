53fbfb646ef6d8fcee86227578e1b1d46dd6a1a9
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2022-05-17 22:09:15 -0500

    actualizaciones 17/05/22

--- RepasosBD3/NotasUpd.Designer.cs
+++ RepasosBD3/NotasUpd.Designer.cs
@@ -30,24 +30,117 @@ namespace RepasosBD3
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotasUpd));
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(364, 33);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Actualizar Nota";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(364, 171);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seleccione Alumno y Nota a cambiar";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(193, 79);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(147, 24);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(18, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(322, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(193, 125);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(147, 24);
+            this.comboBox3.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Nota actual:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Cambiar por:";
             // 
             // NotasUpd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::RepasosBD3.Properties.Resources.head_bg07;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(390, 254);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "NotasUpd";
             this.Text = "Actualizar Nota";
             this.Load += new System.EventHandler(this.NotasUpd_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- RepasosBD3/NotasUpd.cs
+++ RepasosBD3/NotasUpd.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+
 namespace RepasosBD3
 {
     public partial class NotasUpd : Form
@@ -29,7 +31,62 @@ namespace RepasosBD3
 
         private void NotasUpd_Load(object sender, EventArgs e)
         {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT idalumno, nombre FROM alumnos2", form1.cn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
 
+            comboBox1.DataSource = ds.Tables[0];
+            comboBox1.ValueMember = "idalumno";
+            comboBox1.DisplayMember = "nombre";
+
+            CboNotas();
+
+            for (int i = 0; i <= 20; i++)
+            {
+                comboBox3.Items.Add("" + i);
+            }
+
+            comboBox3.SelectedIndex = 14;
+        }
+
+        private void CboNotas()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT idnota, nota FROM notas WHERE idalumno = " + comboBox1.SelectedValue, form1.cn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            comboBox2.DataSource = ds.Tables[0];
+            comboBox2.ValueMember = "idnota";
+            comboBox2.DisplayMember = "nota";
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            CboNotas();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cm = new SqlCommand();
+
+            cm.Connection = form1.cn;
+            cm.CommandText = "UPDATE notas SET nota = " + comboBox3.SelectedIndex + " WHERE idnota = " + comboBox2.SelectedValue;
+            //MessageBox.Show(cm.CommandText); PARA SABER LOS POSIBLES ERRORES AL HACER LA CONSULTA
+            form1.cn.Open();
+            cm.ExecuteNonQuery();
+            form1.cn.Close();
+
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form.GetType() == typeof(NotasQry))
+                {
+                    ((NotasQry)form).consulta();
+                    form.Activate();
+                    form.BringToFront();
+                }
+            }
+
+            this.Dispose();
         }
     }
 }
